dc8578b92ef0431eabc97013fe1b4073048915d4
Christoph Helma <helma@in-silico.de>
2008-10-22 16:17:09 +0200

    installation scripts modified, upgrade scripts added

--- config/environment.rb
+++ config/environment.rb
@@ -71,7 +71,7 @@ end
 
 # Include your application configuration below
 
-ENV['CLASSPATH'] = "#{RAILS_ROOT}/vendor/plugins/opentox/lib/java/:#{RAILS_ROOT}/vendor/plugins/opentox/lib/java/cdk-1.0.1.jar"
+ENV['CLASSPATH'] = "#{RAILS_ROOT}/vendor/plugins/opentox/lib/java/:#{RAILS_ROOT}/vendor/plugins/opentox/lib/java/cdk-1.1.svn-19012008.jar"
 ENV['PATH'] =  "#{RAILS_ROOT}/vendor/bin:" + ENV['PATH']
 ENV['GEM_HOME'] = "#{RAILS_ROOT}/vendor/lib/ruby/gems/1.8"
 ENV['GEM_PATH'] = "#{RAILS_ROOT}/vendor/lib/ruby/gems/1.8"
