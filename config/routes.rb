172f7c93d43e5dcb6b3e568a1654c6643c22f0d6
Christoph Helma <helma@in-silico.de>
2009-06-02 11:55:40 +0200

    README textile numbering removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
 
   # The priority is based upon order of creation: first created -> highest priority.
+  #map.connect ":controller/:action/:id/:smiles"
   map.root :controller => 'models'
   map.resources :models
   map.resources :servers
+
 end
